ca96c1e93b7536b37725c70bf6fcf4f62e29009e
Matheus Carvalho <matheusdocarvalho@gmail.com>
2025-02-20 00:23:51 -0400

    chore: update css variables

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -3,11 +3,11 @@ import { Input } from "@/components/ui/input";
 
 const ProductPage = () => {
   return (
-    <>
+    <div className="flex flex-col gap-4">
       <h1 className="text-red-600">Product Page</h1>
       <Button>Button</Button>
       <Input placeholder="Input" />
-    </>
+    </div>
   );
 };
 
