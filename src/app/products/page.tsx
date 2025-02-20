184e068d18a3b81c908ffd032a9546b61dc79637
Matheus Carvalho <matheusdocarvalho@gmail.com>
2025-02-20 00:22:13 -0400

    feat: add shadcn initial setup and components

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,5 +1,14 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+
 const ProductPage = () => {
-  return <h1>Product Page</h1>;
+  return (
+    <>
+      <h1 className="text-red-600">Product Page</h1>
+      <Button>Button</Button>
+      <Input placeholder="Input" />
+    </>
+  );
 };
 
 export default ProductPage;
