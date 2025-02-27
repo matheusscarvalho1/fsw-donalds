c0d64f2224b826dcd2a94d1bc62d0d5c3bf2a3c2
Matheus Carvalho <matheusdocarvalho@gmail.com>
2025-02-26 20:50:49 -0400

    feat: add initial cart context and cart sheet component

--- src/app/[slug]/menu/[productId]/_components/products-datails.tsx
+++ src/app/[slug]/menu/[productId]/_components/products-datails.tsx
@@ -2,11 +2,14 @@
 import { Prisma } from "@prisma/client";
 import { ChefHatIcon, ChevronLeftIcon, ChevronRightIcon } from "lucide-react";
 import Image from "next/image";
-import { useState } from "react";
+import { useContext, useState } from "react";
 
 import { Button } from "@/components/ui/button";
 import { ScrollArea } from "@/components/ui/scroll-area";
 import { formatCurrency } from "@/helpers/format.currency";
+
+import CartSheet from "../../_components/cart-sheet";
+import { CartContext } from "../../contexts/cart";
 
 interface ProductsDetailsProps {
     product: Prisma.ProductGetPayload<{include: {restaurant: {
@@ -18,6 +21,7 @@ interface ProductsDetailsProps {
 }
 
 const ProductsDetails = ({ product }: ProductsDetailsProps) => {
+    const {toggleCart} = useContext(CartContext)
     const  [quantity, setQuantity] = useState<number>(1);
 
     const handleDecreaseQuantity = () => {
@@ -33,8 +37,13 @@ const ProductsDetails = ({ product }: ProductsDetailsProps) => {
         setQuantity((prev) => prev + 1);
     }
 
+    const handleAddToCart = () =>{
+        toggleCart();
+    }
+
     return (  
-    <div className="relative z-50 mt-[-1.5rem] rounded-t-3xl p-5 flex-auto flex flex-col overflow-hidden">
+    <>
+        <div className="relative z-50 mt-[-1.5rem] rounded-t-3xl p-5 flex-auto flex flex-col overflow-hidden">
         <div className="flex-auto overflow-hidden">
         <div className="flex items-center gap-1.5">
             <Image src={product.restaurant.avatarImageUrl} alt={product.restaurant.name} width={16} height={16} className="rounded-full" />
@@ -73,10 +82,12 @@ const ProductsDetails = ({ product }: ProductsDetailsProps) => {
         </div>
        </ScrollArea>
         </div>
-        <Button className="w-full rounded-full">
+        <Button className="w-full rounded-full" onClick={handleAddToCart}>
             Adicionar à sacola
         </Button>
     </div>
+    <CartSheet />
+    </>
     );
 }
  
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,10 +3,12 @@ import "./globals.css";
 import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 
+import { CartProvider } from "./[slug]/menu/contexts/cart";
+
 const poppins = Poppins({
   weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
   subsets: ["latin"],
-});
+}); 
 
 export const metadata: Metadata = {
   title: "FSW Donalds",
@@ -19,8 +21,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={`${poppins.className} antialiased`}>{children}</body>
+    <html lang="en"> 
+      <body className={`${poppins.className} antialiased`}>
+        <CartProvider>
+        {children}
+        </CartProvider>
+        </body>
     </html>
   );
 }
