e53f15e5ca85d1d1cd703be2c197258d1aa3e95c
Matheus Carvalho <matheusdocarvalho@gmail.com>
2025-03-10 18:35:26 -0400

    fix: ensure user is redirected with cpf after order creation

--- src/app/[slug]/orders/page.tsx
+++ src/app/[slug]/orders/page.tsx
@@ -1,7 +1,7 @@
-//import { isValidCpf, removeCpfPunctuation } from "@/helpers/cpf";
+import { isValidCpf, removeCpfPunctuation } from "@/helpers/cpf";
 import { db } from "@/lib/prisma";
 
-//import CpfForm from "./_components/cpf-form";
+import CpfForm from "./_components/cpf-form";
 import OrderList from "./_components/order-list";
 
 interface OrdersPageProps {
@@ -13,13 +13,16 @@ interface OrdersPageProps {
 const OrdersPage = async ({ searchParams }: OrdersPageProps) => {
     const { cpf } = await searchParams;
 
-    // if (!cpf || !isValidCpf(cpf)) {
-    //     return <CpfForm />
-    // }
+    if (!cpf || !isValidCpf(cpf)) {
+        return <CpfForm />
+    }
 
     const orders = await db.order.findMany ({
+        orderBy: {
+            createdAt: "desc",
+        },
         where: {
-            customerCpf: cpf,
+            customerCpf: removeCpfPunctuation(cpf),
         },
         include: {
             restaurant: {
