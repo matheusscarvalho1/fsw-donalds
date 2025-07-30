10fcbb3f3c91fd4694dffcf6909b803c53debf61
Matheus Carvalho <matheusdocarvalho@gmail.com>
2025-07-30 17:33:52 -0400

    Fix: lint adjustments

--- src/app/[slug]/orders/_components/order-list.tsx
+++ src/app/[slug]/orders/_components/order-list.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { OrderStatus, consumptionMethod, Prisma } from "@prisma/client";
+import { consumptionMethod, OrderStatus, Prisma } from "@prisma/client";
 import { ChevronLeftIcon, ScrollTextIcon } from "lucide-react";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
